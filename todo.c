9905a9e330e6e18ad6f7e7b8534afeb0be84d4b1
anto <antoalasepp@gmail.com>
2023-10-03 22:52:09 +0300

    initial todo loop

--- todo.c
+++ todo.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int nr = 10;
-    printf("todo stuff %d \n", nr);
-
+    char *todo_items[] = {"pick up food", "go home", "make food", "eat"};
+    int i = 0;
+    printf("char size %ld \n", sizeof(char *));
+    printf("todo items size in bytes %ld \n", sizeof(todo_items));
+    printf("todo items size %ld \n", sizeof(todo_items) / sizeof(char *));
+    for (i = 0; i < sizeof(todo_items) / sizeof(char *); i++) {
+        printf("%s \n", todo_items[i]);
+    };
     return 0;
 }
