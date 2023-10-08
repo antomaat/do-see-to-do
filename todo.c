f96eea8ec3e28aa9f82c5b1967f7b9236e6f0611
anto <antoalasepp@gmail.com>
2023-10-08 22:23:07 +0300

    use struct in todo field

--- todo.c
+++ todo.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+struct TodoField {
+    char *message;
+    int priority;
+    int is_done;
+};
+
+struct TodoField *todo_create(char *message, int priority) {
+    struct TodoField *todo = malloc(sizeof(struct TodoField));
+    assert(todo != NULL);
+
+    todo->message = strdup(message);
+    todo->priority = priority;
+    todo->is_done = 0;
+    return todo;
+}
+
+void todo_destroy(struct TodoField *todo) {
+    assert(todo != NULL);
+    free(todo->message);
+    free(todo);
+}
 
 int main() {
-    char *todo_items[] = {"pick up food", "go home", "make food", "eat"};
+    struct TodoField *todo_fields[] = {
+        todo_create("pick up food", 0),
+        todo_create("go home", 1),
+        todo_create("make food", 2),
+        todo_create("eat", 3),
+    };
     int i = 0;
-    printf("char size %ld \n", sizeof(char *));
-    printf("todo items size in bytes %ld \n", sizeof(todo_items));
-    printf("todo items size %ld \n", sizeof(todo_items) / sizeof(char *));
-    for (i = 0; i < sizeof(todo_items) / sizeof(char *); i++) {
-        printf("%s \n", todo_items[i]);
+    for (i = 0; i < sizeof(todo_fields) / sizeof(char *); i++) {
+        printf("%s -> priority %d \n", todo_fields[i]->message, todo_fields[i]->priority);
+    };
+    for (i = 0; i < sizeof(todo_fields) / sizeof(char *); i++) {
+        todo_destroy(todo_fields[i]);
     };
     return 0;
 }
