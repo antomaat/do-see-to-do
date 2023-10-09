a93c3f3becb731684c398060574ad36133ec1aea
anto <antoalasepp@gmail.com>
2023-10-09 22:36:02 +0300

    add function callback

--- todo.c
+++ todo.c
@@ -20,16 +20,31 @@ struct TodoField *todo_create(char *message, int priority) {
 }
 
 void todo_destroy(struct TodoField *todo) {
-    assert(todo != NULL);
-    free(todo->message);
-    free(todo);
+    if (todo != NULL) {
+        free(todo->message);
+        free(todo);
+    }
 }
 
 int is_todo_done(struct TodoField *todo) {
     return todo->is_done;
 }
 
-int (*is_done)(struct TodoField *todo) = is_todo_done;
+int is_todo_not_done(struct TodoField *todo) {
+    return !todo->is_done;
+}
+
+typedef int (*is_done)(struct TodoField *todo);
+
+void print_todo_fields(struct TodoField *todo_fields[], is_done done_fun) {
+    int i = 0;
+    while (todo_fields[i] != NULL) {
+        if (!done_fun(todo_fields[i])) {
+            printf("%s -> priority %d \n", todo_fields[i]->message, todo_fields[i]->priority);
+        }
+        i++;
+    }
+} 
 
 int main() {
     struct TodoField *todo_fields[] = {
@@ -37,16 +52,11 @@ int main() {
         todo_create("go home", 1),
         todo_create("make food", 2),
         todo_create("eat", 3),
+        NULL
     };
 
-    todo_fields[2]->is_done = 1;
-
+    print_todo_fields(todo_fields, is_todo_done);
     int i = 0;
-    for (i = 0; i < sizeof(todo_fields) / sizeof(char *); i++) {
-        if (!is_done(todo_fields[i])) {
-            printf("%s -> priority %d \n", todo_fields[i]->message, todo_fields[i]->priority);
-        }
-    };
     for (i = 0; i < sizeof(todo_fields) / sizeof(char *); i++) {
         todo_destroy(todo_fields[i]);
     };
