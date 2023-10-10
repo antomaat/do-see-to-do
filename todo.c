b5aa689ef20378e0b0dbc4c249f0e2c8a4993521
anto <antoalasepp@gmail.com>
2023-10-10 22:42:05 +0300

    add quiting to program. Simple one at first

--- todo.c
+++ todo.c
@@ -3,6 +3,8 @@
 #include <assert.h>
 #include <string.h>
 
+int contains_string(char input1[], char input2[]);
+
 struct TodoField {
     char *message;
     int priority;
@@ -36,26 +38,65 @@ int is_todo_not_done(struct TodoField *todo) {
 
 typedef int (*is_done)(struct TodoField *todo);
 
-void print_todo_fields(struct TodoField *todo_fields[], is_done done_fun) {
+void print_todo_fields(struct TodoField *todo_fields[]) {
     int i = 0;
     while (todo_fields[i] != NULL) {
-        if (!done_fun(todo_fields[i])) {
+        if (!todo_fields[i]->is_done) {
             printf("%s -> priority %d \n", todo_fields[i]->message, todo_fields[i]->priority);
         }
         i++;
     }
 } 
 
+int process_input(char input[], int input_size) {
+    if (contains_string(input, "q")) {
+        return 1;
+    }    
+    return 0;
+}
+
+int contains_string(char *input1, char *input2) {
+    int index = 0;
+    int contains = 0;
+    while(input1[index] != '\0' && input2[index] != '\0') {
+        if (input1[index] == input2[index]) {
+            contains = 1;
+        } else {
+            contains = 0;
+            break;
+        }
+        index++;
+    }
+    return contains;
+}
+
 int main() {
-    struct TodoField *todo_fields[] = {
+    int todo_index = 0;
+    struct TodoField *todo_fields[50] = {
         todo_create("pick up food", 0),
         todo_create("go home", 1),
         todo_create("make food", 2),
         todo_create("eat", 3),
         NULL
     };
+    todo_index = 4;
 
-    print_todo_fields(todo_fields, is_todo_done);
+    print_todo_fields(todo_fields);
+
+    while(1) {
+        char input[100] = "";
+        fgets(input, 100-1, stdin);
+        int result = process_input(input, 100);
+        if (result == 1) {
+            break;
+        }
+        input[strcspn(input, "\n")] = 0;
+        todo_fields[todo_index] = todo_create(input, todo_index);
+        print_todo_fields(todo_fields);
+        todo_index++;
+    }
+
+    // cleanup
     int i = 0;
     for (i = 0; i < sizeof(todo_fields) / sizeof(char *); i++) {
         todo_destroy(todo_fields[i]);
